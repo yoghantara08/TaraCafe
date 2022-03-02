0fe1bfade0de9df009f6baef1ee3fdcd1b2e43bb
Yoghantara <gyoghantara@gmail.com>
2022-03-02 14:30:07 +0800

    smooth scroll up jquery

--- js/script.js
+++ js/script.js
@@ -42,3 +42,17 @@ function showFooter() {
             </div>
           </div>`;
 }
+
+// Scroll Up
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 40) {
+      $(".go-top-btn").fadeIn();
+    } else {
+      $(".go-top-btn").fadeOut();
+    }
+  });
+  $(".go-top-btn").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 800);
+  });
+});
