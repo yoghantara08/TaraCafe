bc27bee8475a9165442c804bbe7f4f3ef16c99a9
Yoghantara <gyoghantara@gmail.com>
2022-03-25 20:56:27 +0800

    add extra condition for navbar (mobile)

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
-document.addEventListener("scroll", function () {
-  const navbar = document.querySelector("nav");
-  const navToggler = document.getElementById("navToggler");
-  const navLink = document.querySelectorAll(".nav-link");
+const navbar = document.querySelector("nav");
+const navToggler = document.getElementById("navToggler");
+const navLink = document.querySelectorAll(".nav-link");
 
+if (window.innerWidth < 991) {
+  navbar.classList.add("nav-scrolled");
+  navbar.classList.remove("nav-scrolled-text-shadow");
+  navToggler.classList.replace("navbar-dark", "navbar-light");
+  navLink.forEach((e) => e.classList.remove("bawah"));
+}
+
+document.addEventListener("scroll", function () {
   if (this.body.scrollTop > 1 || this.documentElement.scrollTop > 1) {
     navbar.classList.add("nav-scrolled");
     navbar.classList.remove("nav-scrolled-text-shadow");
     navToggler.classList.replace("navbar-dark", "navbar-light");
     navLink.forEach((e) => e.classList.remove("bawah"));
   } else {
-    navbar.classList.add("nav-scrolled-text-shadow");
-    navbar.classList.remove("nav-scrolled");
-    navToggler.classList.replace("navbar-light", "navbar-dark");
-    navLink.forEach((e) => e.classList.add("bawah"));
+    if (window.innerWidth > 992) {
+      navbar.classList.add("nav-scrolled-text-shadow");
+      navbar.classList.remove("nav-scrolled");
+      navToggler.classList.replace("navbar-light", "navbar-dark");
+      navLink.forEach((e) => e.classList.add("bawah"));
+    }
   }
 });
 
